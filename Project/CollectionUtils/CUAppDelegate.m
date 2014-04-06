95c6f8357b5e56428b3e94a8fd5b0a0e1cf2f617
kishikawa katsumi <kishikawakatsumi@mac.com>
2014-04-07 07:16:08 +0900

    Remove debug code.

--- Project/CollectionUtils/CUAppDelegate.m
+++ Project/CollectionUtils/CUAppDelegate.m
@@ -7,40 +7,11 @@
 //
 
 #import "CUAppDelegate.h"
-#import "CollectionUtils.h"
 
 @implementation CUAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-//    NSArray *array = @[@"0",
-//                       @"1",
-//                       [NSNull null],
-//                       @"2",
-//                       [NSNull null],
-//                       @"3"];
-//    NSArray *compactArray = [array cu_compactArray];
-//    NSLog(@"%@", compactArray);
-//    
-//    NSDictionary *dictionary = @{@"one": @"1",
-//                                 @"null": [NSNull null],
-//                                 @"two": @"2",
-//                                 @"three": @"3"};
-//    NSDictionary *compactDictionary = [dictionary cu_compactDictionary];
-//    NSLog(@"%@", compactDictionary);
-    
-NSArray *array = @[@"0",
-                   @"1",
-                   [NSNull null],
-                   @"2",
-                   @{@"zero": @"0",
-                     @"one": @"1",
-                     @"null": [NSNull null],
-                     @"two": @"2"},
-                   @"4"];
-NSMutableArray *compactArray = [[array cu_compactArray] mutableCopy];
-NSLog(@"%@", compactArray);
-    
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     self.window.backgroundColor = [UIColor whiteColor];
